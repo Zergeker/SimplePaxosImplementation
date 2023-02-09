9fe8f3d0b28268e0bda5eb1da38ac4be50316d04
Zergeker <zergec@yandex.ru>
2023-02-09 08:46:09 +0100

    Added configuration for the knowledge of other processes in the system

--- main.go
+++ main.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"example.com/SimplePaxos/paxos"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigFile("config.env")
+	viper.ReadInConfig()
+
 	port := os.Args[1]
 	//0 - proposer, 1 - acceptor, 2 - learner
 	nodeType, _ := strconv.Atoi(os.Args[2])
 	nodeId, _ := strconv.Atoi(os.Args[3])
+	proposers := strings.Split(viper.GetString("PROPOSERS"), ",\n")
+	acceptors := strings.Split(viper.GetString("ACCEPTORS"), ",\n")
+	learners := strings.Split(viper.GetString("LEARNERS"), ",\n")
 
-	n := paxos.NewNode(nodeId, nodeType)
+	n := paxos.NewNode(nodeId, nodeType, proposers, acceptors, learners)
 
 	switch nodeType {
 	case 0:
--- paxos/node.go
+++ paxos/node.go
@@ -1,11 +1,14 @@
 package paxos
 
 type Node struct {
-	NodeId   int
-	NodeType int
+	NodeId    int
+	NodeType  int
+	Proposers []string
+	Acceptors []string
+	Learners  []string
 }
 
-func NewNode(nodeId int, nodeType int) *Node {
-	n := Node{nodeId, nodeType}
+func NewNode(nodeId int, nodeType int, proposers []string, acceptors []string, learners []string) *Node {
+	n := Node{nodeId, nodeType, proposers, acceptors, learners}
 	return &n
 }
